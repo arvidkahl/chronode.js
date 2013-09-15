44248b27f1ea0b4b13b2c1d25e15dec60edff3cd
Arvid Kahl <arvid@koding.com>
2013-09-15 19:48:32 +0200

    heavily changed to allow concurrent npm checks. caching enabled

--- server.js
+++ server.js
@@ -1,38 +1,118 @@
 var http = require('http');
-var fs = require('fs');
 var exec = require('child_process').exec;
 var url = require('url');
 
-var index = 'ohai'
-  // fs.readFileSync('index.html');
+var cache = { initialized : new Date(), items : [] }
+
+var daisy;
+
+daisy = function(args, fn) {
+  return setTimeout(args.next = function() {
+    var f;
+    if ((f = args.shift())) {
+      return !!f(args) || true;
+    } else {
+      return false;
+    }
+  }, 0);
+};
+
+
+fetchTimes =  function(name,callback){
+
+      // console.log('fetchTimes: Checking for module: ',name);
+
+      if(cache.items[name]==null){
+
+        // console.log('fetchTimes: '+name+' not cached. Fetching.');
+
+        child = exec("npm show "+name+" time", function (error, packageTime, stderr) {
+
+          if(error == null) {
+
+              var times = [];
+              lines = packageTime.split('\n');
+
+              for (var line in lines) {
+                time = lines[line].replace(/['",{}]/g,'').match(/([^:]+): ([^:]+)/);
+                if (time && time[1] && time[2]){
+                  times.push({version:time[1].trim(),time:time[2].trim()});
+                }
+              }
+
+              cache.items[name]=times;
+              // console.log('fetchTimes: calling back with new object');
+              callback(times);
+
+            } else {
+              console.log('fetchTimes: ERROR  '+error);
+            }
+          });
+        }
+        else {
+          // console.log('fetchTimes: calling back with old object');
+          // console.log("FetchTimes: "+name+' found in cache.');
+          callback(cache.items[name]);
+        }
+    }
+// main process
 
 http.createServer(function (req, res) {
-  console.log('Getting npm result');
+
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  if (query!=={}) {
-    for (var name in query) {
-      console.log('Checking for module: ',name);
-      child = exec("npm show "+name+" time", function (error, packageTime, stderr) {
-        if(error == null) {
 
-            var times = [];
-            lines = packageTime.split('\n');
-            for (var line in lines) {
-              time = lines[line].replace(/['",{}]/g,'').match(/([^:]+): ([^:]+)/);
-              if (time && time[1] && time[2]){
-                times.push({version:time[1].trim(),time:time[2].trim()});
-              }
-            }
+  // console.log('main: query is '+ query);
 
+  if (Object.keys(query).length !== 0) {
 
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(JSON.stringify(times));
-          } else {
-            console.log(error);
-          }
-      });
+    // console.log('main: Getting npm result');
+
+    var data = {};
+    var queue = [];
+
+    fetchModule = function(name_,callback){
+        // console.log('fetchModule');
+        fetchTimes(name_,function(res){
+          // console.log('fetchModule: Fetching done for '+name_);
+          data[name_]=res;
+          callback(res);
+        });
+      }
+
+    sendResponse = function(data_,callback){
+      // console.log('sendResponse: Done fetching. Sending data.');
+
+      content = 'This is it!'+'<script>var a = '+JSON.stringify(data_)+"</script>";
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+
+      // console.log('Content: ',content);
+      res.end(content);
+      if(callback){callback();}
     }
-  }
+
+    names = [];
+    for (var name in query) {names.push(name)}
+
+    names.forEach( function(name){
+
+      var name_=name.replace(/%s/g,'');
+
+      if(name_.length){
+        queue.push(function(){
+          fetchModule(name_,function(){
+            queue.next();
+          });
+        });
+      }
+    });
+
+    // console.log('Pushing response');
+    queue.push(function(){sendResponse(data)});
+
+    // console.log('running queue');
+    daisy(queue);
+
+    }
 
 }).listen(process.env.PORT || 4000);
